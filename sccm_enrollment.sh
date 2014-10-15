5a9270ee95d62c7ac35227bfd88e518ec3c0f882
Calum <calum.h@gmail.com>
2014-10-15 12:03:09 +1100

    fix whitespace

--- sccm_enrollment.sh
+++ sccm_enrollment.sh
@@ -7,17 +7,13 @@
 ## 
 ## Script to enroll the Mac OS X Device into Production SCCM Environment
 
-
 ## Define some variables
-
 server_address="YOUR SERVER ADDRESS"
 sccmusername="YOUR USERNAME"
 sccmpassword="YOUR PASSWORD"
-cmenroll="/Library/Application Support/Microsoft/CCM/Tools/CMEnroll" # note we dont use a \ to indicate spacing here as expect doesn't use it
+cmenroll="/Library/Application Support/Microsoft/CCM/Tools/CMEnroll" 
 
 ## Now hand off to the expect script to perform the enrollment
-
 /tmp/expect_enrollment.sh $server_address $sccmusername $sccmpassword "$cmenroll"
-
 
 exit 0
